b1f99109689ce92ff534673254449289794d291b
abasaneznanez <abasaneznanez@gmail.com>
2025-09-29 14:59:07 -0600

    Spots Stage 7 initial commit

--- scripts/index.js
+++ scripts/index.js
@@ -3,33 +3,29 @@ const initialCards = [
     name: "Val Thorens",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
   },
-
   {
     name: "Restaurant terrace",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
   },
-
   {
     name: "An outdoor cafe",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
   },
-
   {
     name: "A very long bridge, over the forest and through the trees",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
   },
-
   {
     name: "Tunnel with morning light",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
   },
-
   {
     name: "Mountain house",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
   },
 ];
 
+//  Open and close modal functions
 function openModal(modal) {
   modal.classList.add("modal_is-opened");
 }
@@ -38,48 +34,44 @@ function closeModal(modal) {
   modal.classList.remove("modal_is-opened");
 }
 
+// Elements for the "Edit Profile" modal
 const editProfileBtn = document.querySelector(".profile__edit-btn");
-
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
-//OK 6********** (2nd video)
 const editProfileForm = editProfileModal.querySelector(".modal__form");
-//OK 3*************# para ligarlo con su id =
 const editProfileNameInput = editProfileModal.querySelector(
   "#profile-name-input"
 );
-//OK 4****************** copy //3 and change inside the variable, the word Name for Description
 const editProfileDescriptionInput = editProfileModal.querySelector(
   "#profile-description-input"
 );
-//OK 1****************** class = "" es muy importante
+
 const profileNameEl = document.querySelector(".profile__name");
-//OK 2***************** class = "" es muy importante
 const profileDescriptionEl = document.querySelector(".profile__description");
 
+// Open edit profile modal
 editProfileBtn.addEventListener("click", function () {
-  //OK 5************** I followed the tripleten tutorial video.
   editProfileNameInput.value = profileNameEl.textContent;
-  //OK? 5************** I did this one on my own.
   editProfileDescriptionInput.value = profileDescriptionEl.textContent;
-  openModal(editProfileModal);
+  openModal(editProfileModal); //  Correct modal passed
 });
 
+//  Close edit profile modal
 editProfileCloseBtn.addEventListener("click", function () {
   closeModal(editProfileModal);
 });
-//OK 8********(handler)
+
+//  Handle submit of edit profile form
 function handleEditProfileSubmit(evt) {
   evt.preventDefault();
-  //OK 9*******
   profileNameEl.textContent = editProfileNameInput.value;
-  //10***** (yo lo hice)
   profileDescriptionEl.textContent = editProfileDescriptionInput.value;
   closeModal(editProfileModal);
 }
 
 editProfileForm.addEventListener("submit", handleEditProfileSubmit);
 
+// Elements for the "Add New Post" modal
 const newPostBtn = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
@@ -88,14 +80,17 @@ const addCardFormElement = newPostModal.querySelector(".modal__form");
 const nameInput = newPostModal.querySelector("#card-caption-input");
 const linkInput = newPostModal.querySelector("#card-image-input");
 
+//  Open new post modal
 newPostBtn.addEventListener("click", function () {
   openModal(newPostModal);
 });
 
+//  Close new post modal
 newPostCloseBtn.addEventListener("click", function () {
   closeModal(newPostModal);
 });
 
+//  Handle submit of add card form
 function handleAddCardSubmit(evt) {
   evt.preventDefault();
 
@@ -107,6 +102,7 @@ function handleAddCardSubmit(evt) {
 
 addCardFormElement.addEventListener("submit", handleAddCardSubmit);
 
+//  Log initial cards (no modals used here)
 initialCards.forEach(function (item) {
   console.log(item.name);
   console.log(item.link);
